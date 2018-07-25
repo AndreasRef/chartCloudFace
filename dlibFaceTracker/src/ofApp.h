7df38a82b6be20c104ffaa2324396df894f96de0
AndreasRef <andreasrefsgaard@hotmail.com>
2018-07-25 11:36:46 +0200

    dLib example without ofxFacetracker2

---
+++ dlibFaceTracker/src/ofApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+#include "FaceTracker.h"
+
+
+class FaceAugmented : public ofxDLib::Follower<ofxDLib::Face> {
+protected:
+    ofImage image;
+    ofColor color;
+    ofRectangle roi;
+    ofVec2f cur, smooth;
+    ofPolyline all;
+    ofxDLib::Face face;
+    float startedDying;
+public:
+    FaceAugmented(){
+    };
+    void setup(const ofxDLib::Face & track);
+    void update(const ofxDLib::Face & track);
+    void setImage(const ofPixels & pixels);
+    void kill();
+    void draw();
+    void drawNumbers();
+};
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    ofVideoGrabber video;
+    ofxDLib::FaceTrackerFollower<FaceAugmented> tracker;
+};
