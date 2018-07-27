c1646347878340d0df8c11767dc93d87b9c29365
AndreasRef <andreasrefsgaard@hotmail.com>
2018-07-27 13:00:55 +0200

    Deleted all dlib, added example with bigger input size

---
+++ inputSizeFPStest/src/ofApp.cpp
@@ -0,0 +1,155 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    learned_functions = vector<pfunct_type>(4);
+
+    // Load SVM data model
+    dlib::deserialize(ofToDataPath("data_ecstatic_smile.func")) >> learned_functions[0];
+    dlib::deserialize(ofToDataPath("data_small_smile.func")) >> learned_functions[1];
+    dlib::deserialize(ofToDataPath("data_o.func")) >> learned_functions[2];
+    dlib::deserialize(ofToDataPath("data_neutral.func")) >> learned_functions[3];
+    
+    // Setup value filters for the classifer
+    neutralValue.setFc(0.2);
+    bigSmileValue.setFc(0.2);
+    smallSmileValue.setFc(0.2);
+    oValue.setFc(0.2);
+
+    // All examples share data files from example-data, so setting data path to this folder
+    // This is only relevant for the example apps
+    ofSetDataPathRoot(ofFile(__BASE_FILE__).getEnclosingDirectory()+"../../model/");
+    
+    
+    //we can now get back a list of devices.
+    vector<ofVideoDevice> devices = grabber.listDevices();
+    
+    for(int i = 0; i < devices.size(); i++){
+        if(devices[i].bAvailable){
+            ofLogNotice() << devices[i].id << ": " << devices[i].deviceName;
+        }else{
+            ofLogNotice() << devices[i].id << ": " << devices[i].deviceName << " - unavailable ";
+        }
+    }
+    
+    // Setup grabber
+    grabber.setDeviceID(1);
+    
+    grabber.setDesiredFrameRate(5);
+    grabber.setup(1280,720);
+    
+    
+    // Setup tracker
+    tracker.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    grabber.update();
+    if(grabber.isFrameNew()){
+        tracker.update(grabber);
+        
+        if(tracker.size() > 0){
+            // Run the classifiers and update the filters
+            bigSmileValue.update(learned_functions[0](makeSample()));
+            smallSmileValue.update(learned_functions[1](makeSample()));
+            oValue.update(learned_functions[2](makeSample()));
+            neutralValue.update(learned_functions[3](makeSample()));
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    grabber.draw(0, 0);
+    tracker.drawDebug();
+    
+#ifndef __OPTIMIZE__
+    ofSetColor(ofColor::Red);
+    ofDrawBitmapString("Warning! Run this app in release mode to get proper performance!",10,60);
+    ofSetColor(ofColor::White);
+#endif
+    
+    
+    ofPushMatrix();
+    ofTranslate(0, 100);
+    for (int i = 0; i < 4; i++) {
+        ofSetColor(255);
+        
+        string str;
+        float val;
+        switch (i) {
+            case 0:
+                str = "BIG SMILE";
+                val = bigSmileValue.value();
+                break;
+            case 1:
+                str = "SMALL SMILE";
+                val = smallSmileValue.value();
+                break;
+            case 2:
+                str = "OOO MOUTH";
+                val = oValue.value();
+                break;
+            case 3:
+                str = "NEUTRAL MOUTH";
+                val = neutralValue.value();
+                break;
+        }
+        
+        ofDrawBitmapStringHighlight(str, 20, 0);
+        ofDrawRectangle(20, 20, 300*val, 30);
+        
+        ofNoFill();
+        ofDrawRectangle(20, 20, 300, 30);
+        ofFill();       
+        
+        ofTranslate(0, 70);
+    }
+    
+    ofPopMatrix();
+    
+    // Draw text UI
+    ofDrawBitmapStringHighlight("Framerate : "+ofToString(ofGetFrameRate()), 10, 20);
+    ofDrawBitmapStringHighlight("Tracker thread framerate : "+ofToString(tracker.getThreadFps()), 10, 40);
+
+}
+
+
+// Function that creates a sample for the classifier containing the mouth and eyes
+sample_type ofApp::makeSample(){
+    auto outer = tracker.getInstances()[0].getLandmarks().getImageFeature(ofxFaceTracker2Landmarks::OUTER_MOUTH);
+    auto inner = tracker.getInstances()[0].getLandmarks().getImageFeature(ofxFaceTracker2Landmarks::INNER_MOUTH);
+    
+    auto lEye = tracker.getInstances()[0].getLandmarks().getImageFeature(ofxFaceTracker2Landmarks::LEFT_EYE);
+    auto rEye = tracker.getInstances()[0].getLandmarks().getImageFeature(ofxFaceTracker2Landmarks::RIGHT_EYE);
+    
+    ofVec2f vec = rEye.getCentroid2D() - lEye.getCentroid2D();
+    float rot = vec.angle(ofVec2f(1,0));
+    
+    vector<ofVec2f> relativeMouthPoints;
+    
+    ofVec2f centroid = outer.getCentroid2D();
+    for(ofVec2f p : outer.getVertices()){
+        p -= centroid;
+        p.rotate(rot);
+        p /= vec.length();
+        
+        relativeMouthPoints.push_back(p);
+    }
+    
+    for(ofVec2f p : inner.getVertices()){
+        p -= centroid;
+        p.rotate(rot);
+        p /= vec.length();
+        
+        relativeMouthPoints.push_back(p);
+    }
+    
+    sample_type s;
+    for(int i=0;i<20;i++){
+        s(i*2+0) = relativeMouthPoints[i].x;
+        s(i*2+1) = relativeMouthPoints[i].y;
+    }
+    return s;
+}
