ce2433411fc88837a8757cabd0164fc43dba7163
AndreasRef <andreasrefsgaard@hotmail.com>
2018-07-31 10:31:38 +0200

    Added reasonable "eye+eyebrow openess" indicator

--- angrySadCalculations/src/ofApp.cpp
+++ angrySadCalculations/src/ofApp.cpp
@@ -24,13 +24,11 @@ void ofApp::setup(){
     // Setup grabber
     grabber.setup(1280,720);
     
-    
     // Setup tracker
     tracker.setup();
     
     // open an outgoing connection to HOST:PORT
     sender.setup(HOST, PORT);
-    
     
 }
 
@@ -49,10 +47,21 @@ void ofApp::update(){
         faceDist = tracker.getInstances()[0].getPoseMatrix().getRowAsVec3f(3)[2];//Okay measure for distance between -2000, -10000
         faceDistMapped = ofMap(faceDist, -10000,-2000,0,5);
         
-        float eyeBrowInput = (getGesture(RIGHT_EYEBROW_HEIGHT) + getGesture(LEFT_EYEBROW_HEIGHT) + getGesture(RIGHT_EYE_OPENNESS) + getGesture(LEFT_EYE_OPENNESS)) / faceDistMapped;
+        float eyeBrowInput = ((getGesture(RIGHT_EYEBROW_HEIGHT) + getGesture(LEFT_EYEBROW_HEIGHT) + getGesture(RIGHT_EYE_OPENNESS) + getGesture(LEFT_EYE_OPENNESS)) -0.8);
         filteredEyeBrows.update(eyeBrowInput); //Also add eyes
-
+        
+        cout << "rightEye: ";
+        cout << ofToString(getGesture(RIGHT_EYE_OPENNESS)) << endl;
+        
+        cout << "leftEye: ";
+        cout << ofToString(getGesture(LEFT_EYE_OPENNESS)) << endl;
+        
+        
+        cout << "rightEyebrow: ";
         cout << ofToString(getGesture(RIGHT_EYEBROW_HEIGHT)) << endl;
+        
+        cout << "leftEyebrow: ";
+        cout << ofToString(getGesture(LEFT_EYEBROW_HEIGHT)) << endl;
         
     }
 }
@@ -73,66 +82,69 @@ void ofApp::draw(){
     float val = filteredEyeBrows.value();
     string str = "EYEBROW ADDED: " + ofToString(val);
     
-    
     ofFill();
     ofDrawBitmapStringHighlight(str, 20, 110);
     ofDrawRectangle(20, 120, 300*val, 30);
     
     ofNoFill();
     ofDrawRectangle(20, 120, 300, 30);
-    
 }
 
 
 //--------------------------------------------------------------
 float ofApp:: getGesture (Gesture gesture){
     
-    //Current issues: How to make it scale accordingly
-    
     if(tracker.size()<1) {
         return 0;
     }
     int start = 0, end = 0;
-    int gestureMultiplier = 10;
+    
+    float compareFloat = 1.0;
     
     switch(gesture) {
             // left to right of mouth
         case MOUTH_WIDTH: start = 48; end = 54; break;
             // top to bottom of inner mouth
-        case MOUTH_HEIGHT: start = 51; end = 57; gestureMultiplier = 10; break;
+        case MOUTH_HEIGHT: start = 51; end = 57; break;
             // center of the eye to middle of eyebrow
-        case LEFT_EYEBROW_HEIGHT: start = 38; end = 20; gestureMultiplier = 10; break;
+        case LEFT_EYEBROW_HEIGHT: start = 38; end = 20;
+            
+            compareFloat = tracker.getInstances()[0].getLandmarks().getImagePoint(33).y - tracker.getInstances()[0].getLandmarks().getImagePoint(27).y;
+            
+            break;
             // center of the eye to middle of eyebrow
-        case RIGHT_EYEBROW_HEIGHT: start = 43; end = 23; gestureMultiplier = 10; break;
+        case RIGHT_EYEBROW_HEIGHT: start = 43; end = 23;
+            
+            compareFloat = tracker.getInstances()[0].getLandmarks().getImagePoint(33).y - tracker.getInstances()[0].getLandmarks().getImagePoint(27).y;
+            
+            break;
             // upper inner eye to lower outer eye
-        case LEFT_EYE_OPENNESS: start = 38; end = 40; gestureMultiplier = 25; break;
+        case LEFT_EYE_OPENNESS: start = 38; end = 40;
+            compareFloat = ofDist(
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(36).x,
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(36).y,
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(39).x,
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(39).y
+                                  );
+            break;
             // upper inner eye to lower outer eye
-        case RIGHT_EYE_OPENNESS: start = 43; end = 47; gestureMultiplier = 25; break;
+        case RIGHT_EYE_OPENNESS: start = 43; end = 47;
+            
+            compareFloat = ofDist(
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(42).x,
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(42).y,
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(45).x,
+                                  tracker.getInstances()[0].getLandmarks().getImagePoint(45).y
+                                  );
+            break;
             // nose center to chin center
         case JAW_OPENNESS: start = 33; end = 8; break;
             // left side of nose to right side of nose
         case NOSTRIL_FLARE: start = 31; end = 35; break;
     }
     
-    //    return (1000*abs(tracker.getInstances()[0].getLandmarks().getImagePoint(start).getNormalized().x - tracker.getInstances()[0].getLandmarks().getImagePoint(end).getNormalized().x) + abs(tracker.getInstances()[0].getLandmarks().getImagePoint(start).getNormalized().y - tracker.getInstances()[0].getLandmarks().getImagePoint(end).getNormalized().y));
-    
-    //Attempting to only calculate based on either x or y
-    //Normalized
-    //float noseCompare = tracker.getInstances()[0].getLandmarks().getImagePoint(33).y - tracker.getInstances()[0].getLandmarks().getImagePoint(30).y;
-    
-    
-    float noseCompare = tracker.getInstances()[0].getLandmarks().getImagePoint(33).y - tracker.getInstances()[0].getLandmarks().getImagePoint(27).y;
-    
     float gestureFloat = abs(tracker.getInstances()[0].getLandmarks().getImagePoint(start).y - tracker.getInstances()[0].getLandmarks().getImagePoint(end).y);
-    
-    //cout << ofToString(noseCompare) << endl;
-    
-    //return (gestureMultiplier*abs(abs(tracker.getInstances()[0].getLandmarks().getImagePoint(start).getNormalized().y - tracker.getInstances()[0].getLandmarks().getImagePoint(end).getNormalized().y)));
-    
-    return (gestureFloat/noseCompare);
-    
-    //Not normalized
-    //            return 0.01 * abs(tracker.getInstances()[0].getLandmarks().getImagePoint(start).y - tracker.getInstances()[0].getLandmarks().getImagePoint(end).y);
+    return (gestureFloat/compareFloat);
 }
 
 
